8e10b1e24e6db4d0f817984a052a67914d5d5098
jdkariya <josh.kariya@kellogg.northwestern.edu>
2022-05-06 21:29:29 +0000

    Upated

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
-  
+  resources "tacos"
+end
+
+Rails.application.routes.draw do
+  resources "dice"
+end
+
+Rails.application.routes.draw do
+  resources "cards"
+end
+
+Rails.application.routes.draw do
+  resources "companies"
+end
+
+Rails.application.routes.draw do
+  resources "contacts"
 end
